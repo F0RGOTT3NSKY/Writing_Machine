029737cdffea60ebba83a288185471273820452e
Adrian Gomez <adriangomez2099@gmail.com>
2021-04-13 16:17:42 -0600

    line number working

---
+++ IDE/main.py
@@ -0,0 +1,224 @@
+#                ______________________________ 
+#_______________/ LIBRARIES 
+ 
+from PyQt5.QtWidgets import * 
+from PyQt5.QtCore import * 
+from PyQt5.QtGui import * 
+from PyQt5.Qsci import * 
+ 
+#                ______________________________ 
+#_______________/ CLASS MAIN WINDOW 
+class LineNumberArea(QWidget): 
+    def __init__(self, editor): 
+        QWidget.__init__(self, parent=editor) 
+        self.codeEditor = editor 
+ 
+    def sizeHint(self): 
+        return QSize(self.codeEditor.lineNumberAreaWidth(), 0) 
+ 
+    def paintEvent(self, event): 
+        self.codeEditor.lineNumberAreaPaintEvent(event) 
+         
+class Ui_MainWindow(object): 
+    def setupUi(self, mainWindow): 
+        #           _____________________________ 
+        #__________/ WINDOW CONSTRUCTOR 
+         
+        windowX = 1200 
+        windowY = 900 
+        mainWindow.setFixedSize(windowX, windowY) 
+        mainWindow.setWindowTitle("Wrinting Machine") 
+        #mainWindow.setStyleSheet("background-color : white") 
+        dir_path = r'C:\GitHub' 
+ 
+        #           _____________________________ 
+        #__________/ WINDOW OBJECTS 
+         
+        self.menuBar = QMenuBar(mainWindow)   #Menu Bar 
+        self.menuBar.setGeometry(QRect(0, 0, windowX, 21)) 
+        mainWindow.setMenuBar(self.menuBar) 
+ 
+        self.menuFile = QMenu(self.menuBar)   #Menu File 
+        self.menuFile.setTitle("File") 
+        self.menuBar.addAction(self.menuFile.menuAction()) 
+         
+        self.menuRun = QMenu(self.menuBar)    #Menu Run 
+        self.menuRun.setTitle("Run") 
+        self.menuBar.addAction(self.menuRun.menuAction()) 
+ 
+        self.menuOptions = QMenu(self.menuBar)    #Menu Options 
+        self.menuOptions.setTitle("Options") 
+        self.menuBar.addAction(self.menuOptions.menuAction()) 
+         
+        self.menuHelp = QMenu(self.menuBar)   #Menu Help 
+        self.menuHelp.setTitle("Help") 
+        self.menuBar.addAction(self.menuHelp.menuAction()) 
+ 
+        self.actionNew_File = QAction(mainWindow) #Action New File 
+        self.actionNew_File.setText("New File") 
+        self.menuFile.addAction(self.actionNew_File) 
+         
+        self.actionOpen = QAction(mainWindow) #Action Open  
+        self.actionOpen.setText("Open") 
+        self.menuFile.addAction(self.actionOpen) 
+         
+        self.actionSave = QAction(mainWindow) #Action Save 
+        self.actionSave.setText("Save") 
+        self.menuFile.addAction(self.actionSave) 
+ 
+        self.actionSave_as = QAction(mainWindow)  #Action Save as 
+        self.actionSave_as.setText("Save As...") 
+        self.menuFile.addAction(self.actionSave_as) 
+ 
+        self.actionCompile = QAction(mainWindow)  #Action Compile 
+        self.actionCompile.setText("Compile") 
+        self.menuRun.addAction(self.actionCompile) 
+ 
+        self.actionPreferences = QAction(mainWindow)  #Action Preferences 
+        self.actionPreferences.setText("Preferences") 
+        self.menuOptions.addAction(self.actionPreferences) 
+ 
+        self.actionAbout = QAction(mainWindow)    #Action About 
+        self.actionAbout.setText("About") 
+        self.menuHelp.addAction(self.actionAbout) 
+ 
+        self.actionWiki = QAction(mainWindow) #Action Wiki 
+        self.actionWiki.setText("Wiki") 
+        self.menuHelp.addAction(self.actionWiki) 
+ 
+        self.actionBuild = QAction(mainWindow)    #Action Compile and Run 
+        self.actionBuild.setText("Compile and Run") 
+        self.menuRun.addAction(self.actionBuild) 
+ 
+        self.actionExit = QAction(mainWindow) #Action Exit 
+        self.actionExit.setText("Exit") 
+        self.menuFile.addAction(self.actionExit) 
+ 
+        self.actionSave_and_Exit = QAction(mainWindow)    #Action Save and Exit 
+        self.actionSave_and_Exit.setText("Save and Exit") 
+         
+         
+        self.statusBar = QStatusBar(mainWindow)   #Status Bar 
+        mainWindow.setStatusBar(self.statusBar) 
+        self.statusBar.showMessage("This is status bar") 
+        #self.statusBar.setStyleSheet("background-color : gray") 
+         
+        self.centralWidget = QWidget(mainWindow)  #Central Widget 
+        mainWindow.setCentralWidget(self.centralWidget) 
+         
+        self.groupBox = QGroupBox(self.centralWidget) #Code Group Box 
+        self.groupBox.setGeometry(QRect(5, 5, 850, 670)) 
+        self.groupBox.setTitle("GroupBox") 
+        #self.groupBox.setStyleSheet("background-color : white") 
+ 
+        self.codeText = QPlainTextEdit(self.groupBox) #Code Text 
+        codeEditor = CodeEditor(self.codeText) 
+        self.codeText.setGeometry(QRect(5, 20, 840 ,641)) 
+        codeEditor.setGeometry(QRect(0, 0, 840 ,641)) 
+         
+         
+        self.explorerBox = QGroupBox(self.centralWidget)  #Explorer Group Box 
+        self.explorerBox.setGeometry(QRect(860, 5 , 335, 670)) 
+        self.explorerBox.setTitle("File Explorer") 
+        #self.explorerBox.setStyleSheet("background-color : white") 
+ 
+        self.model = QFileSystemModel(mainWindow) 
+        self.model.setRootPath(dir_path) 
+ 
+        self.treeView = QTreeView(self.explorerBox)   #File Tree View 
+        self.treeView.setGeometry(QRect(5, 20, 325, 641)) 
+        self.treeView.setModel(self.model) 
+        self.treeView.setRootIndex(self.model.index(dir_path)) 
+        self.treeView.setColumnWidth(0, 325) 
+ 
+        self.tabWidget = QTabWidget(self.centralWidget)   #Tab Widget 
+        self.tabWidget.setGeometry(QRect(1, 670, windowX, 189)) 
+        self.tabWidget.setCurrentIndex(0) 
+ 
+        self.Errors_TAB = QWidget()   #Errors Tab 
+        self.tabWidget.addTab(self.Errors_TAB, "Errors") 
+         
+        self.Output_TAB = QWidget()   #Output Tab 
+        self.tabWidget.addTab(self.Output_TAB, "Output") 
+ 
+         
+        QMetaObject.connectSlotsByName(mainWindow) 
+         
+class CodeEditor(QPlainTextEdit): 
+    def __init__(self, parent=None): 
+        QPlainTextEdit.__init__(self, parent) 
+        self.lineNumberArea = LineNumberArea(self) 
+        self.blockCountChanged.connect(self.updateLineNumberAreaWidth) 
+        self.updateRequest.connect(self.updateLineNumberArea) 
+        self.cursorPositionChanged.connect(self.highlightCurrentLine) 
+        self.updateLineNumberAreaWidth(0) 
+        self.highlightCurrentLine() 
+ 
+    def lineNumberAreaPaintEvent(self, event): 
+        painter = QPainter(self.lineNumberArea) 
+        painter.fillRect(event.rect(), Qt.lightGray) 
+ 
+        block = self.firstVisibleBlock() 
+        blockNumber = block.blockNumber(); 
+        top = self.blockBoundingGeometry(block).translated(self.contentOffset()).top() 
+        bottom = top + self.blockBoundingRect(block).height() 
+ 
+        while block.isValid() and top <= event.rect().bottom(): 
+            if block.isVisible() and bottom >= event.rect().top(): 
+                number = str(blockNumber + 1) 
+                painter.setPen(Qt.black) 
+                painter.drawText(0, top, self.lineNumberArea.width(),  
+                    self.fontMetrics().height(), 
+                    Qt.AlignRight, number) 
+            block = block.next() 
+            top = bottom 
+            bottom = top + self.blockBoundingRect(block).height() 
+            blockNumber += 1 
+ 
+    def lineNumberAreaWidth(self): 
+        digits = len(str(self.blockCount())) 
+        space = 3 + self.fontMetrics().width('9')*digits 
+        return space 
+ 
+    def resizeEvent(self, event): 
+        QPlainTextEdit.resizeEvent(self, event) 
+        cr = self.contentsRect() 
+        self.lineNumberArea.setGeometry(QRect(cr.left(), cr.top(), self.lineNumberAreaWidth(), cr.height())) 
+ 
+    @pyqtSlot(int) 
+    def updateLineNumberAreaWidth(self, newBlockCount): 
+        self.setViewportMargins(self.lineNumberAreaWidth(), 0, 0, 0); 
+ 
+    @pyqtSlot() 
+    def highlightCurrentLine(self): 
+        extraSelections = [] 
+        if not self.isReadOnly(): 
+            selection = QTextEdit.ExtraSelection() 
+            lineColor = QColor(Qt.blue).lighter(160) 
+            selection.format.setBackground(lineColor) 
+            selection.format.setProperty(QTextFormat.FullWidthSelection, True) 
+            selection.cursor = self.textCursor() 
+            selection.cursor.clearSelection() 
+            extraSelections.append(selection) 
+        self.setExtraSelections(extraSelections) 
+ 
+    @pyqtSlot(QRect, int) 
+    def updateLineNumberArea(self, rect, dy): 
+        if dy: 
+            self.lineNumberArea.scroll(0, dy) 
+        else: 
+            self.lineNumberArea.update(0, rect.y(), self.lineNumberArea.width(), rect.height()) 
+        if rect.contains(self.viewport().rect()): 
+            self.updateLineNumberAreaWidth(0) 
+ 
+#                ______________________________ 
+#_______________/ START 
+ 
+if __name__ == "__main__": 
+    import sys 
+    app = QApplication(sys.argv) 
+    mainWindow = QMainWindow() 
+    ui = Ui_MainWindow() 
+    ui.setupUi(mainWindow) 
+    mainWindow.show() 
+    sys.exit(app.exec_()) 
